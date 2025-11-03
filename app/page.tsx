b576b8ff4af5d04cc90d8519496443c4a138b524
zhanssss <zhako.bazarov2@gmail.com>
2025-11-03 21:35:24 +0500

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,21 @@ import FeatureCards from "@/components/feature-cards"
 import HowItWorks from "@/components/how-it-works"
 import PricingSection from "@/components/pricing-section"
 import DownloadSection from "@/components/download-section"
+import {Footer} from "react-day-picker";
 
 export default function Home() {
-  return (
-    <div id="home" className="min-h-screen bg-white">
-      <Header />
-      <main>
-        <HeroSection />
-        <FeaturesPreview />
-        <FeatureCards />
-        <HowItWorks />
-        <PricingSection />
-        <DownloadSection />
-      </main>
-    </div>
-  )
+    return (
+        <div id="home" className="min-h-screen bg-white">
+            <Header/>
+            <main>
+                <HeroSection/>
+                <FeaturesPreview/>
+                <FeatureCards/>
+                <HowItWorks/>
+                <PricingSection/>
+                <DownloadSection/>
+            </main>
+            <Footer/>
+        </div>
+    )
 }
