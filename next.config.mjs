17449b3586a1696285554f8a42455b1afccac6cf
sultanjke <s.mecheyev@outlook.com>
2025-11-25 00:46:49 +0500

    update feature cards to use images from AWS S3 for improved performance

--- next.config.mjs
+++ next.config.mjs
@@ -7,9 +7,20 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true,
-    domains: [
-      "api.microlink.io", // Microlink Image Preview
+    deviceSizes: [360, 640, 768, 1024, 1280, 1536],
+    imageSizes: [32, 64, 96, 128, 256, 384],
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "amzn-s3-washly.s3.eu-north-1.amazonaws.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "api.microlink.io",
+        pathname: "/**",
+      },
     ],
   },
 }
